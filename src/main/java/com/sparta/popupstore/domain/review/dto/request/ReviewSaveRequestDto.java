3047e598a9a443607f94e3173caef26e030d7080
11ioo <11ioo@napalt.co.kr>
2024-12-03 16:59:58 +0900

    feat : dto에 엔티티변환기능 추가

--- src/main/java/com/sparta/popupstore/domain/review/dto/request/ReviewSaveRequestDto.java
+++ src/main/java/com/sparta/popupstore/domain/review/dto/request/ReviewSaveRequestDto.java
@@ -1,4 +1,23 @@
 package com.sparta.popupstore.domain.review.dto.request;
 
+import com.sparta.popupstore.domain.popupstore.entity.PopupStore;
+import com.sparta.popupstore.domain.review.entity.Review;
+import com.sparta.popupstore.domain.user.entity.User;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ReviewSaveRequestDto {
+  private String star;
+  private String content;
+
+  public Review toReviewSaveRequestDto(User user,int star, String contents, Long id) {
+    return Review.builder()
+        .user(user)
+        .star(star)
+        .popupStore(PopupStore.builder().id(id).build())
+        .contents(contents)
+        .build();
+  }
 }
