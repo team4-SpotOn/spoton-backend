b61a3082e909290412f2351d588b8e12c98ffbbe
inzulmi <inzulmi132@gmail.com>
2024-12-03 17:31:42 +0900

    [#7] feat: 유저 회원 가입 기능 구현

--- src/main/java/com/sparta/popupstore/domain/user/controller/UserController.java
+++ src/main/java/com/sparta/popupstore/domain/user/controller/UserController.java
@@ -1,12 +1,28 @@
 package com.sparta.popupstore.domain.user.controller;
 
+import com.sparta.popupstore.domain.user.dto.request.UserSignupRequestDto;
+import com.sparta.popupstore.domain.user.dto.response.USerSignupResponseDto;
 import com.sparta.popupstore.domain.user.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<USerSignupResponseDto> signup(@RequestBody @Valid UserSignupRequestDto requestDto) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(userService.signup(requestDto));
+    }
 }
--- src/main/java/com/sparta/popupstore/domain/user/service/UserService.java
+++ src/main/java/com/sparta/popupstore/domain/user/service/UserService.java
@@ -1,5 +1,9 @@
 package com.sparta.popupstore.domain.user.service;
 
+import com.sparta.popupstore.config.PasswordEncoder;
+import com.sparta.popupstore.domain.user.dto.request.UserSignupRequestDto;
+import com.sparta.popupstore.domain.user.dto.response.USerSignupResponseDto;
+import com.sparta.popupstore.domain.user.entity.User;
 import com.sparta.popupstore.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,4 +13,15 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public USerSignupResponseDto signup(UserSignupRequestDto requestDto) {
+        if(userRepository.existsByEmail(requestDto.getEmail())) {
+            throw new RuntimeException("Email address already in use");
+        }
+
+        String encodePassword = passwordEncoder.encode(requestDto.getPassword());
+        User user = requestDto.toEntity(encodePassword);
+        return new USerSignupResponseDto(userRepository.save(user));
+    }
 }
