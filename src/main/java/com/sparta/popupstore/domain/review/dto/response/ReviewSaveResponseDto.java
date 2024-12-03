432b9ad59a06cbb0f171332d797f2bb04dd5a576
11ioo <11ioo@napalt.co.kr>
2024-12-03 17:00:08 +0900

    feat : dto에 엔티티변환기능 추가

--- src/main/java/com/sparta/popupstore/domain/review/dto/response/ReviewSaveResponseDto.java
+++ src/main/java/com/sparta/popupstore/domain/review/dto/response/ReviewSaveResponseDto.java
@@ -1,4 +1,30 @@
 package com.sparta.popupstore.domain.review.dto.response;
 
+import com.sparta.popupstore.domain.popupstore.entity.PopupStore;
+import com.sparta.popupstore.domain.review.entity.Review;
+import com.sparta.popupstore.domain.user.entity.User;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ReviewSaveResponseDto {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String contents;
+  private int star;
+  private String nickname;
+
+  public static ReviewSaveResponseDto createResponseDto(User user, Review review) {
+    ReviewSaveResponseDto responseDto = new ReviewSaveResponseDto();
+    responseDto.id = review.getId();
+    responseDto.star = review.getStar();
+    responseDto.contents = review.getContents();
+    responseDto.nickname = user.getNickname();
+    return responseDto;
+  }
 }
