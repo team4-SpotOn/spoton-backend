b532387fa6273eaccbf65e8ef4ab1446f6b79507
Haseung2 <dlgktmd0@naver.com>
2024-12-03 21:33:09 +0900

    [#12] feat : 이벤트 다건 조회 생성

--- src/main/java/com/sparta/popupstore/domain/promotionalevent/service/PromotionalEventService.java
+++ src/main/java/com/sparta/popupstore/domain/promotionalevent/service/PromotionalEventService.java
@@ -2,10 +2,14 @@ package com.sparta.popupstore.domain.promotionalevent.service;
 
 import com.sparta.popupstore.domain.popupstore.repository.PopupStoreRepository;
 import com.sparta.popupstore.domain.promotionalevent.dto.request.PromotionalEventCreateRequestDto;
+import com.sparta.popupstore.domain.promotionalevent.dto.response.PromotionEventResponseDto;
 import com.sparta.popupstore.domain.promotionalevent.dto.response.PromotionalEventCreateResponseDto;
 import com.sparta.popupstore.domain.promotionalevent.entity.PromotionalEvent;
 import com.sparta.popupstore.domain.promotionalevent.repository.PromotionalEventRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -24,5 +28,10 @@ public class PromotionalEventService {
             popupStoreRepository.findById(popupStoreId).ifPresent(promotionalEvent::addPopupStore);
         }
         return new PromotionalEventCreateResponseDto(promotionalEventRepository.save(promotionalEvent));
+    }
+
+    public Page<PromotionEventResponseDto> findAllPromotionalEvents(int pageNum, int pageSize) {
+        Pageable pageable = PageRequest.of(pageNum-1, pageSize);
+        return promotionalEventRepository.findAllPromotionalEvents(pageable).map(PromotionEventResponseDto::new);
     }
 }
