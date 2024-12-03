aa6c3a4315d7075ff20dcc163f2171eb5fc044c9
kuk1157 <kuk1938@naver.com>
2024-12-03 19:50:32 +0900

    feat: 고객 마이페이지 완료

--- src/main/java/com/sparta/popupstore/domain/user/service/UserService.java
+++ src/main/java/com/sparta/popupstore/domain/user/service/UserService.java
@@ -4,6 +4,8 @@ import com.sparta.popupstore.config.PasswordEncoder;
 import com.sparta.popupstore.domain.user.dto.request.UserSignupRequestDto;
 import com.sparta.popupstore.domain.user.dto.response.USerSignupResponseDto;
 import com.sparta.popupstore.domain.user.entity.User;
+import com.sparta.popupstore.domain.user.dto.response.UserMypageResponseDto;
+import com.sparta.popupstore.domain.user.entity.User;
 import com.sparta.popupstore.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -23,5 +25,14 @@ public class UserService {
         String encodedPassword = passwordEncoder.encode(requestDto.getPassword());
         User user = requestDto.toEntity(encodedPassword);
         return new USerSignupResponseDto(userRepository.save(user));
+    }
+
+
+    // 유저 마이페이지
+    public UserMypageResponseDto getUserMypage(Long userId){
+        User user = userRepository.findById(userId).orElseThrow(() ->
+            new IllegalArgumentException("해당 고객은 존재하지 않습니다.")
+        );
+        return new UserMypageResponseDto(user);
     }
 }
