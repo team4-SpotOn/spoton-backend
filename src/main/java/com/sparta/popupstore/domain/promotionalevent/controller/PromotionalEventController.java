01f44df7817ec5268a2e062654c71931d11cb79d
Haseung2 <dlgktmd0@naver.com>
2024-12-03 21:33:09 +0900

    [#12] feat : 이벤트 다건 조회 생성

--- src/main/java/com/sparta/popupstore/domain/promotionalevent/controller/PromotionalEventController.java
+++ src/main/java/com/sparta/popupstore/domain/promotionalevent/controller/PromotionalEventController.java
@@ -1,6 +1,7 @@
 package com.sparta.popupstore.domain.promotionalevent.controller;
 
 import com.sparta.popupstore.domain.promotionalevent.dto.request.PromotionalEventCreateRequestDto;
+import com.sparta.popupstore.domain.promotionalevent.dto.response.PromotionEventResponseDto;
 import com.sparta.popupstore.domain.promotionalevent.dto.response.PromotionalEventCreateResponseDto;
 import com.sparta.popupstore.domain.promotionalevent.service.PromotionalEventService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -9,6 +10,7 @@ import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -38,5 +40,18 @@ public class PromotionalEventController {
             @RequestParam(required = false, name = "popupStoreId") Long popupStoreId
             ){
         return ResponseEntity.status(HttpStatus.CREATED).body(promotionalEventService.createEvent(promotionalEventCreateRequestDto, popupStoreId));
+    }
+
+    @Operation(summary = "이벤트 다건 조회", description = "현재 등록되어 있는 이벤트들을 보여줍니다. pageNum 과 pageSize 는 default 값이 각각 1과 10입니다.")
+    @Parameters({
+            @Parameter(name = "pageNum", description = "현재 페이지 번호"),
+            @Parameter(name = "pageSize", description = "한 페이지에서 볼 이벤트 갯수")
+    })
+    @GetMapping
+    public ResponseEntity<Page<PromotionEventResponseDto>> findAllPromotionalEvents(
+            @RequestParam(name = "pageNum",required = false, defaultValue = "1") int pageNum,
+            @RequestParam(name = "pageSize",required = false, defaultValue = "10") int pageSize
+    ){
+        return ResponseEntity.ok(promotionalEventService.findAllPromotionalEvents(pageNum, pageSize));
     }
 }
--- src/main/java/com/sparta/popupstore/domain/promotionalevent/repository/PromotionalEventRepository.java
+++ src/main/java/com/sparta/popupstore/domain/promotionalevent/repository/PromotionalEventRepository.java
@@ -1,7 +1,14 @@
 package com.sparta.popupstore.domain.promotionalevent.repository;
 
 import com.sparta.popupstore.domain.promotionalevent.entity.PromotionalEvent;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.nio.channels.FileChannel;
 
 public interface PromotionalEventRepository extends JpaRepository<PromotionalEvent, Long> {
+    @Query(value = "select p from PromotionalEvent p left join fetch p.popupStore")
+    Page<PromotionalEvent> findAllPromotionalEvents(Pageable pageable);
 }
