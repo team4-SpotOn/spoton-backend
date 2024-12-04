c86249115fa58529348635f494ab42d5092fd219
HAEDAL-IRLAB <jyh4282002@naver.com>
2024-12-04 12:00:11 +0900

    [#17] feat: 팝업스토어 생성 기능 추가

--- src/main/java/com/sparta/popupstore/domain/popupstore/controller/PopupStoreController.java
+++ src/main/java/com/sparta/popupstore/domain/popupstore/controller/PopupStoreController.java
@@ -1,12 +1,23 @@
 package com.sparta.popupstore.domain.popupstore.controller;
 
+import com.sparta.popupstore.domain.popupstore.dto.request.PopupStoreCreateRequestDto;
+import com.sparta.popupstore.domain.popupstore.dto.response.PopupStoreCreateResponseDto;
 import com.sparta.popupstore.domain.popupstore.service.PopupStoreService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/popupstores")
 public class PopupStoreController {
 
     private final PopupStoreService popupStoreService;
+
+    @PostMapping
+    public ResponseEntity<PopupStoreCreateResponseDto> createPopupStore(
+            @RequestParam String email,
+            @RequestBody PopupStoreCreateRequestDto requestDto) {
+        return ResponseEntity.ok(popupStoreService.createPopupStore(email, requestDto));
+    }
 }
