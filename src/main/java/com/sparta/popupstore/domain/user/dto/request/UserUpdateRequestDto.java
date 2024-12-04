f7897ab7b809cbfda0c93a3731e2ecc1f6f81539
inzulmi <inzulmi132@gmail.com>
2024-12-04 17:20:35 +0900

    [#49] feat: dto 생성

---
+++ src/main/java/com/sparta/popupstore/domain/user/dto/request/UserUpdateRequestDto.java
@@ -0,0 +1,9 @@
+package com.sparta.popupstore.domain.user.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class UserUpdateRequestDto {
+
+    private String address;
+}
---
+++ src/main/java/com/sparta/popupstore/domain/user/dto/response/UserUpdateResponseDto.java
@@ -0,0 +1,27 @@
+package com.sparta.popupstore.domain.user.dto.response;
+
+import com.sparta.popupstore.domain.user.entity.User;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class UserUpdateResponseDto {
+    private final Long id;
+    private final String email;
+    private final String password;
+    private final String name;
+    private final String address;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime updatedAt;
+
+    public UserUpdateResponseDto(User user) {
+        this.id = user.getId();
+        this.email = user.getEmail();
+        this.password = user.getPassword();
+        this.name = user.getName();
+        this.address = user.getAddress();
+        this.createdAt = user.getCreatedAt();
+        this.updatedAt = user.getUpdatedAt();
+    }
+}
