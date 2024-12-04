c35f5bf6ea6a956bceeaadf3092c028e2a28a499
HAEDAL-IRLAB <jyh4282002@naver.com>
2024-12-04 17:05:47 +0900

    [#17] fix: toEntity 메서드 추가로 인한 코드 변경

--- src/main/java/com/sparta/popupstore/domain/popupstore/service/PopupStoreService.java
+++ src/main/java/com/sparta/popupstore/domain/popupstore/service/PopupStoreService.java
@@ -1,7 +1,6 @@
 package com.sparta.popupstore.domain.popupstore.service;
 
 import com.sparta.popupstore.domain.company.entity.Company;
-import com.sparta.popupstore.domain.company.service.CompanyService;
 import com.sparta.popupstore.domain.popupstore.dto.request.PopupStoreCreateRequestDto;
 import com.sparta.popupstore.domain.popupstore.dto.response.PopupStoreCreateResponseDto;
 import com.sparta.popupstore.domain.popupstore.entity.PopupStore;
@@ -15,31 +14,13 @@ import org.springframework.stereotype.Service;
 public class PopupStoreService {
 
     private final PopupStoreRepository popupStoreRepository;
-    private final CompanyService companyService;
 
 
     @Transactional
-    public PopupStoreCreateResponseDto createPopupStore(String email, PopupStoreCreateRequestDto requestDto) {
-        Company company = companyService.getCompanyByEmail(email);
+    public PopupStoreCreateResponseDto createPopupStore(Company company, PopupStoreCreateRequestDto requestDto) {
         if (company == null) {
             throw new RuntimeException("Unauthorized access - company not found");
         }
-
-        PopupStore popupStore = PopupStore.builder()
-                .company(company)
-                .name(requestDto.getName())
-                .contents(requestDto.getContent())
-                .image(requestDto.getImageUrl())
-                .price(Integer.parseInt(requestDto.getPrice()))
-                .address(requestDto.getAddress())
-                .startDate(requestDto.getStartDate())
-                .endDate(requestDto.getEndDate())
-                .startTime(requestDto.getStartTime())
-                .endTime(requestDto.getEndTime())
-                .build();
-
-        PopupStore savedPopupStore = popupStoreRepository.save(popupStore);
-
-        return new PopupStoreCreateResponseDto(savedPopupStore);
+        return new PopupStoreCreateResponseDto(popupStoreRepository.save(requestDto.toEntity(company)));
     }
 }
