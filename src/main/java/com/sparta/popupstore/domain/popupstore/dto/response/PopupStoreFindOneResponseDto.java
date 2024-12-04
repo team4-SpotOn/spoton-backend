b1d24bd99895cf50c452f0ceea8995dc1df83c70
HAEDAL-IRLAB <jyh4282002@naver.com>
2024-12-04 21:36:59 +0900

    [#19] add: 팝업스토어 단건조회 dto 추가

---
+++ src/main/java/com/sparta/popupstore/domain/popupstore/dto/response/PopupStoreFindOneResponseDto.java
@@ -0,0 +1,42 @@
+package com.sparta.popupstore.domain.popupstore.dto.response;
+
+import com.sparta.popupstore.domain.popupstore.entity.PopupStore;
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.time.LocalDate;
+import lombok.Getter;
+
+@Getter
+public class PopupStoreFindOneResponseDto {
+
+    @Schema(description = "팝업스토어 이름")
+    private String name;
+
+    @Schema(description = "팝업스토어 시작일")
+    private LocalDate startDate;
+
+    @Schema(description = "팝업스토어 종료일")
+    private LocalDate endDate;
+
+    @Schema(description = "팝업스토어 입장료")
+    private int price;
+
+    @Schema(description = "팝업스토어 내용")
+    private String contents;
+
+    @Schema(description = "팝업스토어 이미지")
+    private String image;
+
+    @Schema(description = "팝업스토어 주소")
+    private String address;
+
+    public PopupStoreFindOneResponseDto(PopupStore popupStore) {
+        this.name = popupStore.getName();
+        this.startDate = popupStore.getStartDate();
+        this.endDate = popupStore.getEndDate();
+        this.price = popupStore.getPrice();
+        this.contents = popupStore.getContents();
+        this.image = popupStore.getImage();
+        this.address = popupStore.getAddress();
+    }
+
+}
