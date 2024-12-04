7581925904707a0696ecf6568b7088eb49a6cb7d
Haseung2 <dlgktmd0@naver.com>
2024-12-04 15:38:45 +0900

    [#15] feat : 이벤트 단건조회 기능 구현
    PopupStoreResponseDTO 와 EventResponseDto 를 두개로 분리하였다.

---
+++ src/main/java/com/sparta/popupstore/domain/promotionevent/dto/response/PromotionEventFindOneResponseDto.java
@@ -0,0 +1,38 @@
+package com.sparta.popupstore.domain.promotionevent.dto.response;
+
+import com.sparta.popupstore.domain.promotionevent.entity.PromotionEvent;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PromotionEventFindOneResponseDto {
+    @Schema(description = "프로모션 이벤트 고유 번호")
+    private final Long id;
+    @Schema(description = "프로모션 이벤트 제목")
+    private final String title;
+    @Schema(description = "프로모션 이벤트 설명")
+    private final String description;
+    @Schema(description = "팝업스토어 객체")
+    private final PromotionEventFindOnePopupStoreResponseDto promotionEventFindPopupStoreResponseDto;
+    @Schema(description = "할인 율")
+    private final int discountPercentage;
+    @Schema(description = "총 쿠폰 갯수")
+    private final int totalCount;
+    @Schema(description = "프로모션 이벤트 시작일")
+    private final LocalDateTime startDateTime;
+    @Schema(description = "프로모션 이벤트 종료일")
+    private final LocalDateTime endDateTime;
+
+    public PromotionEventFindOneResponseDto(PromotionEvent promotionEvent) {
+        this.id = promotionEvent.getId();
+        this.title = promotionEvent.getTitle();
+        this.description = promotionEvent.getDescription();
+        this.promotionEventFindPopupStoreResponseDto = promotionEvent.getPopupStore() != null ? new PromotionEventFindOnePopupStoreResponseDto(promotionEvent.getPopupStore()) : null;
+        this.discountPercentage = promotionEvent.getDiscountPercentage();
+        this.totalCount = promotionEvent.getTotalCount();
+        this.startDateTime = promotionEvent.getStartDateTime();
+        this.endDateTime = promotionEvent.getEndDateTime();
+    }
+}
