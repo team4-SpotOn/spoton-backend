17e2e149ad5f986a6ab3637658e1e84a38a2ac93
HAEDAL-IRLAB <jyh4282002@naver.com>
2024-12-04 17:05:28 +0900

    [#17] fix: price 자료형 int로 변경 및 toEntity 메서드 추가

--- src/main/java/com/sparta/popupstore/domain/popupstore/dto/request/PopupStoreCreateRequestDto.java
+++ src/main/java/com/sparta/popupstore/domain/popupstore/dto/request/PopupStoreCreateRequestDto.java
@@ -1,20 +1,38 @@
 package com.sparta.popupstore.domain.popupstore.dto.request;
 
+import com.sparta.popupstore.domain.company.entity.Company;
+import com.sparta.popupstore.domain.popupstore.entity.PopupStore;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 @Getter
+@Setter
 public class PopupStoreCreateRequestDto {
     private String name;
     private String content;
     private String imageUrl;
-    private String price;
+    private int price;
     private String address;
     private LocalDate startDate;
     private LocalDate endDate;
     private LocalTime startTime;
     private LocalTime endTime;
 
+    public PopupStore toEntity(Company company) {
+        return PopupStore.builder()
+                .company(company)
+                .name(this.name)
+                .contents(this.content)
+                .image(this.imageUrl)
+                .price(this.price)
+                .address(this.address)
+                .startDate(this.startDate)
+                .endDate(this.endDate)
+                .startTime(this.startTime)
+                .endTime(this.endTime)
+                .build();
+    }
 }
