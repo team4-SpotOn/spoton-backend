c8756a7a57d5b6b3e508b26d54fcfe88d17cf5f4
inzulmi <inzulmi132@gmail.com>
2024-12-04 16:35:24 +0900

    [#40] feat: 옵티마이저

--- src/main/java/com/sparta/popupstore/domain/company/controller/CompanyController.java
+++ src/main/java/com/sparta/popupstore/domain/company/controller/CompanyController.java
@@ -22,11 +22,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
