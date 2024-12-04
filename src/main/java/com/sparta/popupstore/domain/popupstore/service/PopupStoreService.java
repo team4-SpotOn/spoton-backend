c63a2c2cd724ab386e9340c10b7642702a818dc1
HAEDAL-IRLAB <jyh4282002@naver.com>
2024-12-04 21:30:49 +0900

    [#18] feat: 팝업 스토어 수정 기능 추가 (관리자 수정 미구현

--- src/main/java/com/sparta/popupstore/domain/popupstore/service/PopupStoreService.java
+++ src/main/java/com/sparta/popupstore/domain/popupstore/service/PopupStoreService.java
@@ -2,7 +2,10 @@ package com.sparta.popupstore.domain.popupstore.service;
 
 import com.sparta.popupstore.domain.company.entity.Company;
 import com.sparta.popupstore.domain.popupstore.dto.request.PopupStoreCreateRequestDto;
+import com.sparta.popupstore.domain.popupstore.dto.request.PopupStoreUpdateRequestDto;
 import com.sparta.popupstore.domain.popupstore.dto.response.PopupStoreCreateResponseDto;
+import com.sparta.popupstore.domain.popupstore.dto.response.PopupStoreUpdateResponseDto;
+import com.sparta.popupstore.domain.popupstore.entity.PopupStore;
 import com.sparta.popupstore.domain.popupstore.repository.PopupStoreRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +16,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
 
 @Service
 @RequiredArgsConstructor
@@ -25,7 +29,6 @@ public class PopupStoreService {
     @Transactional
     public PopupStoreCreateResponseDto createPopupStore(Company company, PopupStoreCreateRequestDto requestDto, MultipartFile imageFile) throws IOException {
         String imagePath = saveImageFile(imageFile);
-
         return new PopupStoreCreateResponseDto(popupStoreRepository.save(requestDto.toEntity(company, imagePath)));
     }
 
@@ -38,5 +41,38 @@ public class PopupStoreService {
         Files.createDirectories(path.getParent());
         Files.write(path, file.getBytes());
         return path.toString();
+    }
+
+    @Transactional
+    public PopupStoreUpdateResponseDto updatePopupStore(Long popupId, Company company, PopupStoreUpdateRequestDto requestDto, MultipartFile imageFile) {
+        PopupStore popupStore = popupStoreRepository.findById(popupId)
+                .orElseThrow(() -> new RuntimeException("PopupStore not found"));
+
+        if (!popupStore.getCompany().equals(company)) {
+            throw new RuntimeException("Unauthorized access - not the owner of the popup store");
+        }
+
+        if (!isEditable(popupStore)) {
+            throw new RuntimeException("Cannot edit a popup store that is in progress");
+            // todo : 관리자인 경우 수정 가능하도록 로직 구현해야함
+        }
+
+        requestDto.updatePopupStore(popupStore);
+
+        if (imageFile != null && !imageFile.isEmpty()) {
+            String imagePath = null;
+            try {
+                imagePath = saveImageFile(imageFile);
+            } catch (IOException e) {
+                throw new RuntimeException("not found image file");
+            }
+            popupStore.setImage(imagePath);
+        }
+
+        return new PopupStoreUpdateResponseDto(popupStoreRepository.save(popupStore));
+    }
+
+    private boolean isEditable(PopupStore popupStore) {
+        return popupStore.getStartDate().isAfter(LocalDate.now());
     }
 }
