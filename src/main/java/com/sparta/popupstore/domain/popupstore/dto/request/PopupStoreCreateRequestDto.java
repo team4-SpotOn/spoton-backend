3979976789285c303129bba126fa0a4a2458b86f
HAEDAL-IRLAB <jyh4282002@naver.com>
2024-12-04 19:30:21 +0900

    [#17] fix: validation 수정

--- src/main/java/com/sparta/popupstore/domain/popupstore/dto/request/PopupStoreCreateRequestDto.java
+++ src/main/java/com/sparta/popupstore/domain/popupstore/dto/request/PopupStoreCreateRequestDto.java
@@ -2,6 +2,8 @@ package com.sparta.popupstore.domain.popupstore.dto.request;
 
 import com.sparta.popupstore.domain.company.entity.Company;
 import com.sparta.popupstore.domain.popupstore.entity.PopupStore;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,21 +13,29 @@ import java.time.LocalTime;
 @Getter
 @Setter
 public class PopupStoreCreateRequestDto {
+    @NotNull(message = "팝업스토어 이름을 입력해주세요.")
     private String name;
+    @NotNull(message = "팝업스토어 내용을 입력해주세요.")
     private String content;
+    @Min(value = 0, message = "가격은 0이상이어야만 합니다.")
     private int price;
+    @NotNull(message = "팝업스토어 주소를 입력해주세요.")
     private String address;
+    @NotNull(message = "팝업스토어 시작일을 입력해주세요.")
     private LocalDate startDate;
+    @NotNull(message = "팝업스토어 종료일을 입력해주세요.")
     private LocalDate endDate;
+    @NotNull(message = "팝업스토어 개장시간을 입력해주세요.")
     private LocalTime startTime;
+    @NotNull(message = "팝업스토어 폐장시간을 입력해주세요.")
     private LocalTime endTime;
 
-    public PopupStore toEntity(Company company, String imageUrl) {
+    public PopupStore toEntity(Company company, String imagePath) {
         return PopupStore.builder()
                 .company(company)
                 .name(this.name)
                 .contents(this.content)
-                .image(imageUrl)
+                .image(imagePath)
                 .price(this.price)
                 .address(this.address)
                 .startDate(this.startDate)
--- src/main/java/com/sparta/popupstore/domain/popupstore/entity/PopupStore.java
+++ src/main/java/com/sparta/popupstore/domain/popupstore/entity/PopupStore.java
@@ -3,8 +3,6 @@ package com.sparta.popupstore.domain.popupstore.entity;
 import com.sparta.popupstore.domain.common.entity.BaseEntity;
 import com.sparta.popupstore.domain.company.entity.Company;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -24,31 +22,20 @@ public class PopupStore extends BaseEntity {
 
     @ManyToOne
     @JoinColumn(name = "company_id")
-    @NotNull(message = "회사를 입력해주세요.")
     private Company company;
-
-    @NotNull(message = "팝업스토어 이름을 입력해주세요.")
     private String name;
-    @NotNull(message = "팝업스토어 내용을 입력해주세요.")
     private String contents;
-    @NotNull(message = "팝업스토어 이미지를 추가해주세요.")
     private String image;
-    @Min(value = 0, message = "가격은 0이상이어야만 합니다.")
     private int price;
-    @NotNull(message = "팝업스토어 주소 입력해주세요.")
     private String address; // 위도와 경도로 주소 저장.
-    @NotNull(message = "팝업스토어 시작일 입력해주세요.")
     private LocalDate startDate;
-    @NotNull(message = "팝업스토어 종료일 입력해주세요.")
     private LocalDate endDate;
-    @NotNull(message = "팝업스토어 개장시간 입력해주세요.")
     private LocalTime startTime;
-    @NotNull(message = "팝업스토어 폐장시간 입력해주세요.")
     private LocalTime endTime;
 
 
     @Builder
-    public PopupStore(Long id, Company company, String name,  String contents, String image, int price, String address, LocalDate startDate, LocalDate endDate, LocalTime startTime, LocalTime endTime) {
+    public PopupStore(Long id, Company company, String name, String contents, String image, int price, String address, LocalDate startDate, LocalDate endDate, LocalTime startTime, LocalTime endTime) {
         this.id = id;
         this.company = company;
         this.name = name;
