14c58426e8a1afac877b160a42175a3ae4d5bb87
11ioo <11ioo@napalt.co.kr>
2024-12-04 15:01:19 +0900

    feat : 리뷰삭제기능 예외처리 추가

--- src/main/java/com/sparta/popupstore/domain/review/dto/request/ReviewCreateRequestDto.java
+++ src/main/java/com/sparta/popupstore/domain/review/dto/request/ReviewCreateRequestDto.java
@@ -3,12 +3,16 @@ package com.sparta.popupstore.domain.review.dto.request;
 import com.sparta.popupstore.domain.popupstore.entity.PopupStore;
 import com.sparta.popupstore.domain.review.entity.Review;
 import com.sparta.popupstore.domain.user.entity.User;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class ReviewCreateRequestDto {
+  @DecimalMax(value = "5.0", message = "1~5사이의 별점을 입력해주세요.")
+  @DecimalMin(value = "1.0", message = "1~5사이의 별점을 입력해주세요.")
   private int star;
   private String content;
 
--- src/main/java/com/sparta/popupstore/domain/review/service/ReviewService.java
+++ src/main/java/com/sparta/popupstore/domain/review/service/ReviewService.java
@@ -1,12 +1,11 @@
 package com.sparta.popupstore.domain.review.service;
 
-import com.sparta.popupstore.domain.popupstore.entity.PopupStore;
 import com.sparta.popupstore.domain.popupstore.repository.PopupStoreRepository;
-import com.sparta.popupstore.domain.review.dto.request.ReviewCreateRequestDto;
 import com.sparta.popupstore.domain.review.dto.response.ReviewCreateResponseDto;
 import com.sparta.popupstore.domain.review.entity.Review;
 import com.sparta.popupstore.domain.review.repository.ReviewRepository;
 import com.sparta.popupstore.domain.user.entity.User;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +17,8 @@ public class ReviewService {
     private final PopupStoreRepository popupStoreRepository;
 
     public ReviewCreateResponseDto deleteReview(User user, Long reviewId){
-        Review review = reviewRepository.findById(reviewId).orElseThrow();
+        Review review = reviewRepository.findById(reviewId)
+            .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 리뷰입니다."));
         reviewRepository.delete(review);
         return new ReviewCreateResponseDto(review);
     }
