71f8abb261e0e709a9b1f52b84f11bf087d9a402
HAEDAL-IRLAB <jyh4282002@naver.com>
2024-12-05 11:50:41 +0900

    [#18] feat : 유효성검사 추가

--- src/main/java/com/sparta/popupstore/domain/popupstore/dto/request/PopupStoreUpdateRequestDto.java
+++ src/main/java/com/sparta/popupstore/domain/popupstore/dto/request/PopupStoreUpdateRequestDto.java
@@ -13,8 +13,10 @@ public class PopupStoreUpdateRequestDto {
     @NotBlank(message = "팝업스토어 이름을 입력해주세요.")
     private String name;
     @NotBlank(message = "팝업스토어 시작날짜를 입력해주세요.")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate startDate;
     @NotBlank(message = "팝업스토어 종료날짜 입력해주세요.")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate endDate;
     @NotBlank(message = "팝업스토어 개장시간 입력해주세요.")
     @DateTimeFormat(pattern = "HH:mm")
