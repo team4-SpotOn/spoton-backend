05e73eb76bbe4184587ab461e0e4584f781035fe
HAEDAL-IRLAB <jyh4282002@naver.com>
2024-12-05 11:49:39 +0900

    [#18] feat : 유효성검사 추가

--- src/main/java/com/sparta/popupstore/domain/popupstore/dto/request/PopupStoreUpdateRequestDto.java
+++ src/main/java/com/sparta/popupstore/domain/popupstore/dto/request/PopupStoreUpdateRequestDto.java
@@ -1,21 +1,32 @@
 package com.sparta.popupstore.domain.popupstore.dto.request;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 @Getter
 public class PopupStoreUpdateRequestDto {
+    @NotBlank(message = "팝업스토어 이름을 입력해주세요.")
     private String name;
+    @NotBlank(message = "팝업스토어 시작날짜를 입력해주세요.")
     private LocalDate startDate;
+    @NotBlank(message = "팝업스토어 종료날짜 입력해주세요.")
     private LocalDate endDate;
+    @NotBlank(message = "팝업스토어 개장시간 입력해주세요.")
+    @DateTimeFormat(pattern = "HH:mm")
     private LocalTime startTime;
+    @NotBlank(message = "팝업스토어 폐장시간 입력해주세요.")
+    @DateTimeFormat(pattern = "HH:mm")
     private LocalTime endTime;
     @Min(value = 0, message = "가격은 0이상이어야만 합니다.")
     private String price;
+    @NotBlank(message = "팝업스토어 내용을 입력해주세요.")
     private String content;
     private String imagePath;
+    @NotBlank(message = "팝업스토어 주소를 입력해주세요.")
     private String address;
 }
