5396fa0c1795cc919feb74e1d4f533fc2a4dbe80
HAEDAL-IRLAB <jyh4282002@naver.com>
2024-12-05 10:57:26 +0900

    [#18] fix : 업데이트 메서드 엔티티로 이동 및 개점시간 폐점시간 추가

--- src/main/java/com/sparta/popupstore/domain/popupstore/dto/request/PopupStoreUpdateRequestDto.java
+++ src/main/java/com/sparta/popupstore/domain/popupstore/dto/request/PopupStoreUpdateRequestDto.java
@@ -1,27 +1,21 @@
 package com.sparta.popupstore.domain.popupstore.dto.request;
 
-import com.sparta.popupstore.domain.popupstore.entity.PopupStore;
 import jakarta.validation.constraints.Min;
 import lombok.Getter;
+
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Getter
 public class PopupStoreUpdateRequestDto {
     private String name;
     private LocalDate startDate;
     private LocalDate endDate;
+    private LocalTime startTime;
+    private LocalTime endTime;
     @Min(value = 0, message = "가격은 0이상이어야만 합니다.")
     private String price;
     private String content;
-    private String imageUrl;
+    private String imagePath;
     private String address;
-
-    public void updatePopupStore(PopupStore popupStore) {
-        if (this.name != null) popupStore.setName(this.name);
-        if (this.startDate != null) popupStore.setStartDate(this.startDate);
-        if (this.endDate != null) popupStore.setEndDate(this.endDate);
-        if (this.price != null) popupStore.setPrice(Integer.parseInt(this.price));
-        if (this.content != null) popupStore.setContents(this.content);
-        if (this.address != null) popupStore.setAddress(this.address);
-    }
 }
