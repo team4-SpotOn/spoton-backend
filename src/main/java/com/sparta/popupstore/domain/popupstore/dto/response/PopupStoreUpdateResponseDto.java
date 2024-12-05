a1dae4c6b3bc2559be9c29c879da706cd78064aa
HAEDAL-IRLAB <jyh4282002@naver.com>
2024-12-05 10:58:16 +0900

    [#18] fix : 변수명 변경 및 개점시간 폐점시간 추가

--- src/main/java/com/sparta/popupstore/domain/popupstore/dto/response/PopupStoreUpdateResponseDto.java
+++ src/main/java/com/sparta/popupstore/domain/popupstore/dto/response/PopupStoreUpdateResponseDto.java
@@ -4,24 +4,29 @@ import com.sparta.popupstore.domain.popupstore.entity.PopupStore;
 import lombok.Getter;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Getter
 public class PopupStoreUpdateResponseDto {
     private String name;
     private LocalDate startDate;
     private LocalDate endDate;
+    private LocalTime startTime;
+    private LocalTime endTime;
     private int price;
     private String content;
-    private String imageUrl;
+    private String imagePath;
     private String address;
 
     public PopupStoreUpdateResponseDto(PopupStore popupStore) {
         this.name = popupStore.getName();
         this.startDate = popupStore.getStartDate();
         this.endDate = popupStore.getEndDate();
+        this.startTime = popupStore.getStartTime();
+        this.endTime = popupStore.getEndTime();
         this.price = popupStore.getPrice();
         this.content = popupStore.getContents();
-        this.imageUrl = popupStore.getImage();
+        this.imagePath = popupStore.getImage();
         this.address = popupStore.getAddress();
     }
 }
