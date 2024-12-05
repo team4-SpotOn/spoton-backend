28c327bc8583754eb40c1ed8368abaa49dd173be
HAEDAL-IRLAB <jyh4282002@naver.com>
2024-12-05 09:46:40 +0900

    [#19] feat: 관리자 - 팝업스토어 수정 api 추가

--- src/main/java/com/sparta/popupstore/domain/popupstore/controller/PopupStoreController.java
+++ src/main/java/com/sparta/popupstore/domain/popupstore/controller/PopupStoreController.java
@@ -1,12 +1,15 @@
 package com.sparta.popupstore.domain.popupstore.controller;
 
 import com.sparta.popupstore.domain.common.annotation.AuthCompany;
+import com.sparta.popupstore.domain.common.annotation.AuthUser;
 import com.sparta.popupstore.domain.company.entity.Company;
 import com.sparta.popupstore.domain.popupstore.dto.request.PopupStoreCreateRequestDto;
 import com.sparta.popupstore.domain.popupstore.dto.request.PopupStoreUpdateRequestDto;
 import com.sparta.popupstore.domain.popupstore.dto.response.PopupStoreCreateResponseDto;
 import com.sparta.popupstore.domain.popupstore.dto.response.PopupStoreUpdateResponseDto;
 import com.sparta.popupstore.domain.popupstore.service.PopupStoreService;
+import com.sparta.popupstore.domain.user.entity.User;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -31,7 +34,20 @@ public class PopupStoreController {
         return ResponseEntity.ok(popupStoreService.createPopupStore(company, requestDto, imageFile));
     }
 
+    @Operation(summary = "관리자 - 팝업 스토어 수정")
+    @PatchMapping("/admin/{popupId}")
+    public ResponseEntity<PopupStoreUpdateResponseDto> updatePopupStore(
+            @PathVariable Long popupId,
+            @AuthUser User user,
+            @RequestPart("requestDto") @Valid PopupStoreUpdateRequestDto requestDto,
+            @RequestPart(value = "imageFile", required = false) MultipartFile imageFile
+    ) {
+        PopupStoreUpdateResponseDto responseDto = null;
+        responseDto = popupStoreService.updatePopupStore(popupId, user, requestDto, imageFile);
+        return ResponseEntity.ok(responseDto);
+    }
 
+    @Operation(summary = "회사 - 팝업 스토어 수정")
     @PatchMapping("/{popupId}")
     public ResponseEntity<PopupStoreUpdateResponseDto> updatePopupStore(
             @PathVariable Long popupId,
