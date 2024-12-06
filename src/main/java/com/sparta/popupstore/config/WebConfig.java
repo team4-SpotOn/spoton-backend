dbb0bbb8c3561a7b2373b909b2ca309f6946870a
inzulmi <inzulmi132@gmail.com>
2024-12-06 20:48:02 +0900

    [#62] feat: 유저 권한 인증 인터셉터 구현

--- src/main/java/com/sparta/popupstore/config/WebConfig.java
+++ src/main/java/com/sparta/popupstore/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.sparta.popupstore.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -13,10 +14,16 @@ public class WebConfig implements WebMvcConfigurer {
 
     private final AuthUserResolver authUserResolver;
     private final AuthCompanyResolver authCompanyResolver;
+    private final CheckRoleInterceptor checkRoleInterceptor;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(authUserResolver);
         resolvers.add(authCompanyResolver);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(checkRoleInterceptor);
     }
 }
