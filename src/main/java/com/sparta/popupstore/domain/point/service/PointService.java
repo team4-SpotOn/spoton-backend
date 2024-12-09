eb1ece92ed07a05a9dc3057d744c23c7ecdb0988
11ioo <11ioo@napalt.co.kr>
2024-12-09 19:26:28 +0900

    fix : 예약한 스토어에만 댓글 달수있도록 수정

--- src/main/java/com/sparta/popupstore/domain/point/service/PointService.java
+++ src/main/java/com/sparta/popupstore/domain/point/service/PointService.java
@@ -1,9 +1,7 @@
 package com.sparta.popupstore.domain.point.service;
 
-import com.sparta.popupstore.domain.point.entity.PointChargedLog;
 import com.sparta.popupstore.domain.point.repository.PointChargedLogRepository;
 import com.sparta.popupstore.domain.point.repository.PointUsedLogRepository;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
