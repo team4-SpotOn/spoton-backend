6f3a470bff4b02930b4f956d52eed41c217ad573
11ioo <11ioo@napalt.co.kr>
2024-12-09 19:54:04 +0900

    fix : Point dto 삭제

--- src/main/java/com/sparta/popupstore/domain/point/dto/request/PointUsedLogRequestDto.java
+++
@@ -1,5 +0,0 @@
-package com.sparta.popupstore.domain.point.dto.request;
-
-public class PointUsedLogRequestDto {
-
-}
--- src/main/java/com/sparta/popupstore/domain/point/dto/response/PointChargedLogResponseDto.java
+++
@@ -1,5 +0,0 @@
-package com.sparta.popupstore.domain.point.dto.response;
-
-public class PointChargedLogResponseDto {
-
-}
