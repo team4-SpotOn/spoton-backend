78e12cb28cb200e30b17f98382e2817656e7fb6a
HAEDAL-IRLAB <jyh4282002@naver.com>
2024-12-10 15:31:54 +0900

    [#97] rebase : 충돌 해결

--- src/main/java/com/sparta/popupstore/domain/popupstore/dto/response/PopupStoreCreateResponseDto.java
+++ src/main/java/com/sparta/popupstore/domain/popupstore/dto/response/PopupStoreCreateResponseDto.java
@@ -3,6 +3,7 @@ package com.sparta.popupstore.domain.popupstore.dto.response;
 import com.sparta.popupstore.domain.common.entity.Address;
 import com.sparta.popupstore.domain.popupstore.entity.PopupStore;
 import io.swagger.v3.oas.annotations.media.Schema;
+import com.sparta.popupstore.domain.popupstore.entity.PopupStoreOperating;
 import lombok.Getter;
 
 import java.time.LocalDate;
@@ -24,14 +25,18 @@ public class PopupStoreCreateResponseDto {
     private final LocalDate endDate;
     @Schema(description = "생성된 팝업스토어 이미지 리스트")
     private final List<PopupStoreImageResponseDto> imageList;
+    @Schema(description = "생성된 팝업스토어 운영시간")
+    private List<PopupStoreOperating> popupStoreOperatingList;
 
-    public PopupStoreCreateResponseDto(PopupStore popupStore) {
+
+    public PopupStoreCreateResponseDto(PopupStore popupStore, List<PopupStoreOperating> popupStoreOperatingList) {
         this.name = popupStore.getName();
         this.content = popupStore.getContents();
         this.price = popupStore.getPrice();
         this.address = popupStore.getAddress();
         this.startDate = popupStore.getStartDate();
         this.endDate = popupStore.getEndDate();
+        this.popupStoreOperatingList = popupStoreOperatingList;
         this.imageList = popupStore.getPopupStoreImageList().stream().map(PopupStoreImageResponseDto::new).toList();
     }
 }
