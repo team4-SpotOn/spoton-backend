661eafcfc009336dc665c01efef37e54bf7d798b
HAEDAL-IRLAB <jyh4282002@naver.com>
2024-12-10 19:51:22 +0900

    [#98] fix : 유저가 있을 경우와 없을 경우 로직 병합

--- src/main/java/com/sparta/popupstore/domain/popupstore/controller/PopupStoreController.java
+++ src/main/java/com/sparta/popupstore/domain/popupstore/controller/PopupStoreController.java
@@ -62,19 +62,13 @@ public class PopupStoreController {
 
     @Operation(summary = "팝업 스토어 단건 조회", description = "팝업스토어 단건조회(상세보기)")
     @GetMapping("/popupstores/{popupStoreId}")
-    public ResponseEntity<PopupStoreFindOneResponseDto> getPopupStoreFindOne(@PathVariable Long popupStoreId) {
-        return ResponseEntity.ok(popupStoreService.getPopupStoreOne(popupStoreId));
-    }
-
-    @Operation(summary = "팝업 스토어 단건 조회", description = "팝업스토어 단건조회(상세보기)")
-    @GetMapping("/popupstores/{popupStoreId}")
     public ResponseEntity<PopupStoreFindOneResponseDto> getPopupStoreFindOne(
             @PathVariable Long popupStoreId,
             @AuthUser User user,
             HttpServletRequest request,
             HttpServletResponse response
     ) {
-        return ResponseEntity.ok(popupStoreService.getPopupStoreOne(popupStoreId, request, response));
+        return user != null ? ResponseEntity.ok(popupStoreService.getPopupStoreOne(popupStoreId, request, response)) : ResponseEntity.ok(popupStoreService.getPopupStoreOne(popupStoreId));
     }
 
     @Operation(summary = "회사 - 팝업스토어 삭제", description = "popupStoreId에 해당하는 팝업스토어를 삭제합니다.")
