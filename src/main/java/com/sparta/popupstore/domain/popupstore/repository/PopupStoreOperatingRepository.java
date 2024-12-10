513f775789f387cf5a72a565fd9eda2fbea0f9bc
HAEDAL-IRLAB <jyh4282002@naver.com>
2024-12-10 16:49:44 +0900

    [#97] feat : PopupStoreOperatingRepository 추가

---
+++ src/main/java/com/sparta/popupstore/domain/popupstore/repository/PopupStoreOperatingRepository.java
@@ -0,0 +1,7 @@
+package com.sparta.popupstore.domain.popupstore.repository;
+
+import com.sparta.popupstore.domain.popupstore.entity.PopupStoreOperating;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PopupStoreOperatingRepository extends JpaRepository<PopupStoreOperating, Long> {
+}
