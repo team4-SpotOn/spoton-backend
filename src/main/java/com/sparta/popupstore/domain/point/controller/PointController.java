3c6947788a10d5ff973ee500fcca9f3287310e43
11ioo <11ioo@napalt.co.kr>
2024-12-11 10:23:01 +0900

    [#88]feat : 포인트 서버와 컨트롤러 뼈대 추가

--- src/main/java/com/sparta/popupstore/domain/point/controller/PointController.java
+++ src/main/java/com/sparta/popupstore/domain/point/controller/PointController.java
@@ -1,15 +1,37 @@
 package com.sparta.popupstore.domain.point.controller;
 
+import com.sparta.popupstore.domain.common.annotation.AuthUser;
+import com.sparta.popupstore.domain.point.entity.PointChargedLog;
+import com.sparta.popupstore.domain.point.entity.PointUsedLog;
 import com.sparta.popupstore.domain.point.service.PointService;
+import com.sparta.popupstore.domain.user.entity.User;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/reviews")
+@RequestMapping("/user/point")
 @RequiredArgsConstructor
-@Tag(name = "Review", description = "리뷰 관련 API")
+@Tag(name = "Point", description = "포인트 관련 API")
 public class PointController {
   private final PointService pointService;
+
+  @PostMapping("/charge")
+  public PointChargedLog pointCharge(@AuthUser User user,@RequestBody PointChargedLog pointChargedLog) {
+    return null;
+  }
+
+  @GetMapping("/charge")
+  public PointChargedLog pointChargeLoge(@AuthUser User user,@RequestBody PointChargedLog pointChargedLog) {
+    return null;
+  }
+
+  @GetMapping("/use")
+  public PointUsedLog pointUsedLog(@AuthUser User user,@RequestBody PointUsedLog pointUsedLog) {
+    return null;
+  }
 }
--- src/main/java/com/sparta/popupstore/domain/point/service/PointService.java
+++ src/main/java/com/sparta/popupstore/domain/point/service/PointService.java
@@ -1,7 +1,11 @@
 package com.sparta.popupstore.domain.point.service;
 
+import com.sparta.popupstore.domain.point.entity.PointChargedLog;
+import com.sparta.popupstore.domain.point.entity.PointUsedLog;
 import com.sparta.popupstore.domain.point.repository.PointChargedLogRepository;
 import com.sparta.popupstore.domain.point.repository.PointUsedLogRepository;
+import com.sparta.popupstore.domain.user.entity.User;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,4 +15,15 @@ public class PointService {
 
     private final PointChargedLogRepository pointChargedLogRepository;
     private final PointUsedLogRepository pointUsedLogRepository;
+
+    public PointChargedLog pointCharge(User user, PointChargedLog pointChargedLog) {
+        return null;
+    }
+
+    public List<PointChargedLog> pointChargeLog() {
+        return null;
+    }
+    public List<PointUsedLog> pointUsedLogs() {
+        return null;
+    }
 }
