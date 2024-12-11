d111b73f7128bd32935426b7cc01a0b81987f5bb
inzulmi <inzulmi132@gmail.com>
2024-12-11 11:04:10 +0900

    [#72] style: 변수 이름 수정, 주석 삭제

--- src/main/java/com/sparta/popupstore/domain/oauth2/controller/OAuth2SigninController.java
+++ src/main/java/com/sparta/popupstore/domain/oauth2/controller/OAuth2SigninController.java
@@ -19,27 +19,25 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class OAuth2SigninController {
 
-    private final OAuth2SigninService oAuth2LoginService;
+    private final OAuth2SigninService oAuth2SigninService;
     private final JwtUtil jwtUtil;
 
-    // 1. 로그인 페이지로
     @GetMapping("/oauth2/signin/{provider}")
     public void redirectSigninPage(
             @PathVariable OAuth2Provider provider,
             HttpServletResponse response
     ) throws IOException {
-        String loginPageUrl = oAuth2LoginService.generateSigninPageUrl(provider);
-        response.sendRedirect(loginPageUrl);
+        String signinPageUrl = oAuth2SigninService.generateSigninPageUrl(provider);
+        response.sendRedirect(signinPageUrl);
     }
 
-    // 2. callback, code 받아서 token 받아오는 method
     @GetMapping("/oauth2/callback/{provider}")
     public ResponseEntity<Void> callback(
             @PathVariable OAuth2Provider provider,
             @RequestParam(name = "code") String authorizationCode,
             HttpServletResponse response
     ) {
-        SocialUser socialUser = oAuth2LoginService.signin(provider, authorizationCode);
+        SocialUser socialUser = oAuth2SigninService.signin(provider, authorizationCode);
         jwtUtil.addJwtToCookie(socialUser.getEmail(), response);
         return ResponseEntity
                 .status(HttpStatus.OK)
