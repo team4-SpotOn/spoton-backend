4df840e1bf5838129462975cea718b0b727092a6
HAEDAL-IRLAB <jyh4282002@naver.com>
2024-12-11 16:31:34 +0900

    [#97] feat : 해당 팝업스토어를 갖진 운영시간을 삭제하는 기능 추가

--- src/main/java/com/sparta/popupstore/domain/popupstore/repository/PopupStoreOperatingRepository.java
+++ src/main/java/com/sparta/popupstore/domain/popupstore/repository/PopupStoreOperatingRepository.java
@@ -1,7 +1,15 @@
 package com.sparta.popupstore.domain.popupstore.repository;
 
+import com.sparta.popupstore.domain.popupstore.entity.PopupStore;
 import com.sparta.popupstore.domain.popupstore.entity.PopupStoreOperating;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PopupStoreOperatingRepository extends JpaRepository<PopupStoreOperating, Long> {
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM PopupStoreOperating po WHERE po.popupStore = :popupStore")
+    void deleteByPopupStore(PopupStore popupStore);
 }
