d72b3f576d99f84cfb7d78c55e2d351989e6fbb7
HAEDAL-IRLAB <jyh4282002@naver.com>
2024-12-12 17:05:58 +0900

    [#97] refactor : 반환 형식 변경

--- src/main/java/com/sparta/popupstore/domain/popupstore/dto/response/PopupStoreCreateResponseDto.java
+++ src/main/java/com/sparta/popupstore/domain/popupstore/dto/response/PopupStoreCreateResponseDto.java
@@ -6,7 +6,10 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import com.sparta.popupstore.domain.popupstore.entity.PopupStoreOperating;
 import lombok.Getter;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.HashMap;
 import java.util.List;
 
 @Getter
@@ -26,16 +29,27 @@ public class PopupStoreCreateResponseDto {
     @Schema(description = "생성된 팝업스토어 이미지 리스트")
     private final List<PopupStoreImageResponseDto> imageList;
     @Schema(description = "생성된 팝업스토어 운영시간")
-    private final List<PopupStoreOperating> operatingList;
+    private final List<HashMap<DayOfWeek, LocalTime>> operatingList;
 
-    public PopupStoreCreateResponseDto(PopupStore popupStore, List<PopupStoreOperating> popupStoreOperatingList) {
+    public PopupStoreCreateResponseDto(PopupStore popupStore, List<PopupStoreOperating> operatingList) {
         this.name = popupStore.getName();
         this.contents = popupStore.getContents();
         this.price = popupStore.getPrice();
         this.address = popupStore.getAddress();
         this.startDate = popupStore.getStartDate();
         this.endDate = popupStore.getEndDate();
-        this.operatingList = popupStoreOperatingList;
+        this.operatingList = processOperatingList(operatingList);
         this.imageList = popupStore.getPopupStoreImageList().stream().map(PopupStoreImageResponseDto::new).toList();
+    }
+
+    private List<HashMap<DayOfWeek, LocalTime>> processOperatingList(List<PopupStoreOperating> operatingList) {
+        return operatingList.stream()
+                .map(operating -> {
+                    HashMap<DayOfWeek, LocalTime> map = new HashMap<>();
+                    map.put(operating.getDayOfWeek(), operating.getStartTime());
+                    map.put(operating.getDayOfWeek(), operating.getEndTime());
+                    return map;
+                })
+                .toList();
     }
 }
