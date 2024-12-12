f5d25cd3e16dea23aba5ea6be5dd6f0044155a85
inzulmi <inzulmi132@gmail.com>
2024-12-12 10:56:48 +0900

    [#73] feat: naver response dto 추가

--- src/main/java/com/sparta/popupstore/domain/oauth2/client/naver/dto/NaverUserInfoResponse.java
+++ src/main/java/com/sparta/popupstore/domain/oauth2/client/naver/dto/NaverUserInfoResponse.java
@@ -5,12 +5,10 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.sparta.popupstore.domain.oauth2.client.common.OAuth2UserInfo;
 import com.sparta.popupstore.domain.oauth2.type.OAuth2Provider;
 
-import java.util.Map;
-
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class NaverUserInfoResponse extends OAuth2UserInfo {
 
-    public NaverUserInfoResponse(Long id, Map<String, Object> attributes) {
-        super(OAuth2Provider.NAVER, id.toString(), (String) attributes.get("email"));
+    public NaverUserInfoResponse(String message, NaverUserResponse response) {
+        super(OAuth2Provider.NAVER, response.getId(), response.getName());
     }
 }
