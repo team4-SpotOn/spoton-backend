7cf63c3d899b07d5db8a3aac7d76c580eee104ff
HAEDAL-IRLAB <jyh4282002@naver.com>
2024-12-13 16:50:47 +0900

    [#120] fix : 파일명 변경 PopupStoreAttributes -> PopupStoreAttribute PopupStoreAttribute -> PopupStoreAttributeEnum

--- src/main/java/com/sparta/popupstore/domain/popupstore/entity/PopupStoreAttribute.java
+++ src/main/java/com/sparta/popupstore/domain/popupstore/entity/PopupStoreAttribute.java
@@ -1,8 +1,28 @@
 package com.sparta.popupstore.domain.popupstore.entity;
 
-public enum PopupStoreAttribute {
-    PARKING,
-    RESERVATION
-    ;
+import jakarta.persistence.*;
+import lombok.*;
 
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "popupstore_attributes")
+public class PopupStoreAttribute {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "popupstore_id")
+    private PopupStore popupStore;
+
+    @Enumerated(EnumType.STRING)
+    private PopupStoreAttributeEnum attribute;
+    private Boolean isAllow;
+
+    public PopupStoreAttribute(PopupStore popupStore, PopupStoreAttributeEnum attribute, Boolean isAllow) {
+        this.popupStore = popupStore;
+        this.attribute = attribute;
+        this.isAllow = isAllow;
+    }
 }
