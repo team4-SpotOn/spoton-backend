d31f2bf007e7149413bcb3d53732b75cf35b5414
HAEDAL-IRLAB <jyh4282002@naver.com>
2024-12-13 16:51:15 +0900

    [#120] fix : id를 통해 속성을 찾는 것이 아닌 PopupStore 엔티티를 사용해서 속성을 찾도록 메서드 변경

--- src/main/java/com/sparta/popupstore/domain/popupstore/repository/PopupStoreAttributeRepository.java
+++ src/main/java/com/sparta/popupstore/domain/popupstore/repository/PopupStoreAttributeRepository.java
@@ -1,12 +1,12 @@
 package com.sparta.popupstore.domain.popupstore.repository;
 
 import com.sparta.popupstore.domain.popupstore.entity.PopupStore;
-import com.sparta.popupstore.domain.popupstore.entity.PopupStoreAttributes;
+import com.sparta.popupstore.domain.popupstore.entity.PopupStoreAttribute;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface PopupStoreAttributeRepository extends JpaRepository<PopupStoreAttributes, Long> {
-    void deleteByPopupStoreId(Long id);
-    List<PopupStoreAttributes> findByPopupStoreId(Long popupStoreId);
+public interface PopupStoreAttributeRepository extends JpaRepository<PopupStoreAttribute, Long> {
+    void deleteByPopupStore(PopupStore popupStore);
+    List<PopupStoreAttribute> findByPopupStore(PopupStore popupStore);
 }
