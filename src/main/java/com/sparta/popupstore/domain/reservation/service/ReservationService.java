7155391daa8a76977a3cf362f77fc28a6c549cf5
HAEDAL-IRLAB <jyh4282002@naver.com>
2024-12-15 14:49:43 +0900

    [#94] feat : 팝업스토어 예약 여부 확인 추가

--- src/main/java/com/sparta/popupstore/domain/reservation/service/ReservationService.java
+++ src/main/java/com/sparta/popupstore/domain/reservation/service/ReservationService.java
@@ -3,6 +3,9 @@ package com.sparta.popupstore.domain.reservation.service;
 import com.sparta.popupstore.domain.common.exception.CustomApiException;
 import com.sparta.popupstore.domain.common.exception.ErrorCode;
 import com.sparta.popupstore.domain.popupstore.entity.PopupStore;
+import com.sparta.popupstore.domain.popupstore.entity.PopupStoreAttribute;
+import com.sparta.popupstore.domain.popupstore.entity.PopupStoreAttributeEnum;
+import com.sparta.popupstore.domain.popupstore.repository.PopupStoreAttributeRepository;
 import com.sparta.popupstore.domain.popupstore.repository.PopupStoreRepository;
 import com.sparta.popupstore.domain.reservation.entity.Reservation;
 import com.sparta.popupstore.domain.reservation.repository.ReservationRepository;
@@ -14,6 +17,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -21,7 +26,7 @@ public class ReservationService {
 
     private final ReservationRepository reservationRepository;
     private final PopupStoreRepository popupStoreRepository;
-    private final UserRepository userRepository;
+    private final PopupStoreAttributeRepository popupStoreAttributeRepository;
 
     @Transactional
     public Reservation createReservation(Long popupStoreId, User user, LocalDateTime reservationAt) {
@@ -33,9 +38,18 @@ public class ReservationService {
         PopupStore popupStore = popupStoreRepository.findById(popupStoreId)
                 .orElseThrow(() -> new CustomApiException(ErrorCode.POPUP_STORE_NOT_FOUND));
 
-        if (user.getPoint() < popupStore.getPrice()) {
-            throw new CustomApiException(ErrorCode.INSUFFICIENT_POINTS);
+        List<PopupStoreAttribute> attributes = popupStoreAttributeRepository.findByPopupStore(popupStore);
+
+        PopupStoreAttributeEnum target = PopupStoreAttributeEnum.RESERVATION;
+        Optional<PopupStoreAttribute> userOptional = attributes.stream()
+                .filter(attribute -> attribute.getAttribute().equals(target))
+                .findFirst();
+
+        if (userOptional.isPresent()) {
+            if(!userOptional.get().getIsAllow()) throw new CustomApiException(ErrorCode.POPUP_STORE_CAN_NOT_RESERVATION);
         }
+
+        if (user.getPoint() < popupStore.getPrice()) throw new CustomApiException(ErrorCode.INSUFFICIENT_POINTS);
 
         user.decreasePoint(popupStore.getPrice());
 
