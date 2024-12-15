e7c4230af624a6c888c1ebd2dd79a96060ab3d6f
HAEDAL-IRLAB <jyh4282002@naver.com>
2024-12-15 13:35:31 +0900

    [#94] feat : 팝업스토어 예약 기능 추가

--- src/main/java/com/sparta/popupstore/domain/reservation/controller/ReservationController.java
+++ src/main/java/com/sparta/popupstore/domain/reservation/controller/ReservationController.java
@@ -1,12 +1,27 @@
 package com.sparta.popupstore.domain.reservation.controller;
 
+import com.sparta.popupstore.domain.common.annotation.AuthUser;
+import com.sparta.popupstore.domain.reservation.entity.Reservation;
 import com.sparta.popupstore.domain.reservation.service.ReservationService;
+import com.sparta.popupstore.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/popupstores")
 public class ReservationController {
 
     private final ReservationService reservationService;
+
+    @PostMapping("/{popupStoreId}/reservation")
+    public ResponseEntity<Reservation> reservePopupStore(
+            @PathVariable Long popupStoreId,
+            @AuthUser User user,
+            @RequestParam LocalDateTime reservationAt) {
+        return ResponseEntity.ok(reservationService.createReservation(popupStoreId, user, reservationAt));
+    }
 }
