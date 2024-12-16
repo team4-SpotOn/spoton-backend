3076ebe82bafc7e9a91f72411fe7d48b0f0ef55a
inzulmi <inzulmi132@gmail.com>
2024-12-16 09:43:08 +0900

    [#143] fix: json 입력 변환을 위한 어노테이션 추가

--- src/main/java/com/sparta/popupstore/domain/kakaoaddress/dto/Document.java
+++ src/main/java/com/sparta/popupstore/domain/kakaoaddress/dto/Document.java
@@ -1,9 +1,13 @@
 package com.sparta.popupstore.domain.kakaoaddress.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Document {
     // road_address 필드
     @JsonProperty("road_address")
--- src/main/java/com/sparta/popupstore/domain/kakaoaddress/dto/RoadAddress.java
+++ src/main/java/com/sparta/popupstore/domain/kakaoaddress/dto/RoadAddress.java
@@ -2,9 +2,13 @@ package com.sparta.popupstore.domain.kakaoaddress.dto;
 
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class RoadAddress {
     @JsonProperty("address_name")
     private String addressName; // 도로명 주소
