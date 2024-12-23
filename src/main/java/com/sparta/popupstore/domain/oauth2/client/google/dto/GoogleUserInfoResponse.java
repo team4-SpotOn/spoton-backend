5ed1c15a9f1306849bcfbae31d9390c4f56916b9
inzulmi <inzulmi132@gmail.com>
2024-12-23 17:24:54 +0900

    [#192] refactor: 소셜 로그인 로직 수정

--- src/main/java/com/sparta/popupstore/domain/oauth2/client/google/dto/GoogleUserInfoResponse.java
+++ src/main/java/com/sparta/popupstore/domain/oauth2/client/google/dto/GoogleUserInfoResponse.java
@@ -1,11 +1,14 @@
 package com.sparta.popupstore.domain.oauth2.client.google.dto;
 
+import com.fasterxml.jackson.annotation.JsonSetter;
 import com.sparta.popupstore.domain.oauth2.client.common.OAuth2UserInfo;
 import com.sparta.popupstore.domain.oauth2.type.OAuth2Platform;
 
 public class GoogleUserInfoResponse extends OAuth2UserInfo {
 
-    public GoogleUserInfoResponse(String id, Object email) {
-        super(OAuth2Platform.GOOGLE, id);
+    @JsonSetter("id")
+    public void setUserInfo(String id) {
+        this.platform = OAuth2Platform.GOOGLE;
+        this.platformId = id;
     }
 }
--- src/main/java/com/sparta/popupstore/domain/oauth2/client/naver/dto/NaverUserInfoResponse.java
+++ src/main/java/com/sparta/popupstore/domain/oauth2/client/naver/dto/NaverUserInfoResponse.java
@@ -1,11 +1,14 @@
 package com.sparta.popupstore.domain.oauth2.client.naver.dto;
 
+import com.fasterxml.jackson.annotation.JsonSetter;
 import com.sparta.popupstore.domain.oauth2.client.common.OAuth2UserInfo;
 import com.sparta.popupstore.domain.oauth2.type.OAuth2Platform;
 
 public class NaverUserInfoResponse extends OAuth2UserInfo {
 
-    public NaverUserInfoResponse(NaverUserResponse response, Object message) {
-        super(OAuth2Platform.NAVER, response.getId());
+    @JsonSetter("response")
+    public void setUserInfo(NaverUserResponse response) {
+        this.platform = OAuth2Platform.NAVER;
+        this.platformId = response.getId();
     }
 }
