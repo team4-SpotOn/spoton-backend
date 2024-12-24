75333b0c8e44435720efa71a091cba6ec970ced4
11ioo <11ioo@napalt.co.kr>
2024-12-24 16:03:24 +0900

    [#196] fix: findByMonth에 오버라이드 추가

--- src/main/java/com/sparta/popupstore/domain/popupstore/repository/PopupStoreQueryDslImpl.java
+++ src/main/java/com/sparta/popupstore/domain/popupstore/repository/PopupStoreQueryDslImpl.java
@@ -42,6 +42,7 @@ public class PopupStoreQueryDslImpl implements PopupStoreQueryDsl {
                 .fetch();
     }
 
+    @Override
     public List<PopupStore> findByMonth(LocalDate startDate, LocalDate endDate, Long page, Long size) {
         YearMonth MontDate = YearMonth.now();
         startDate = MontDate.atDay(1);
