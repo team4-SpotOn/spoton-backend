357af41904497de18b546899bae97dfc097c2110
inzulmi <inzulmi132@gmail.com>
2024-12-27 16:29:58 +0900

    [#217] refactor: NaverUserResponse 삭제

--- src/main/java/com/sparta/popupstore/domain/oauth2/client/naver/dto/NaverUserInfoDto.java
+++ src/main/java/com/sparta/popupstore/domain/oauth2/client/naver/dto/NaverUserInfoDto.java
@@ -6,6 +6,8 @@ import com.sparta.popupstore.domain.oauth2.type.OAuth2Platform;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.Map;
+
 @Getter
 @NoArgsConstructor
 public class NaverUserInfoDto extends OAuth2UserInfoDto {
@@ -13,7 +15,7 @@ public class NaverUserInfoDto extends OAuth2UserInfoDto {
     private final OAuth2Platform platform = OAuth2Platform.NAVER;
 
     @JsonSetter("response")
-    public void setPlatformId(NaverUserResponse response) {
-        this.platformId = response.getId();
+    public void setPlatformId(Map<String, Object> response) {
+        this.platformId = (String) response.get("id");
     }
 }
