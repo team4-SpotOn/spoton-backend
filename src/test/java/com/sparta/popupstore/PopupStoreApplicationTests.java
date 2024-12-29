cd05a167456115fb633ffac5acb8663966c77f17
kuk1157 <kuk1938@naver.com>
2024-12-29 17:21:16 +0900

    [#228] fix: 테스트 코드 수정 - @SpringBootTest 삭제

--- src/test/java/com/sparta/popupstore/PopupStoreApplicationTests.java
+++ src/test/java/com/sparta/popupstore/PopupStoreApplicationTests.java
@@ -1,9 +1,10 @@
 package com.sparta.popupstore;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@DataJpaTest
 class PopupStoreApplicationTests {
 
 	@Test
--- src/test/java/com/sparta/popupstore/domain/coupon/service/CouponServiceTest.java
+++ src/test/java/com/sparta/popupstore/domain/coupon/service/CouponServiceTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,7 +27,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-@SpringBootTest
+@DataJpaTest
 public class CouponServiceTest {
 
     @Autowired
