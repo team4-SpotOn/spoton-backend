d90ebb5d15f3ba2192f947b2e282e2a5a9aa24c4
kuk1157 <kuk1938@naver.com>
2024-12-29 17:44:50 +0900

    [#228] fix: 테스트 코드 수정

--- src/test/java/com/sparta/popupstore/PopupStoreApplicationTests.java
+++ src/test/java/com/sparta/popupstore/PopupStoreApplicationTests.java
@@ -7,8 +7,10 @@ import com.sparta.popupstore.domain.user.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 class PopupStoreApplicationTests {
 
 	@Autowired
