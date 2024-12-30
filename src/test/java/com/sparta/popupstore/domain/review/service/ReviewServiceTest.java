a036e53e7921c88df98eea2b021b3af58dc8583d
kuk1157 <kuk1938@naver.com>
2024-12-30 16:27:57 +0900

    [#242] 리뷰 테스트 코드 수정 - @SpringBootTest 사용 가능하도록 수정

--- src/test/java/com/sparta/popupstore/domain/review/service/ReviewServiceTest.java
+++ src/test/java/com/sparta/popupstore/domain/review/service/ReviewServiceTest.java
@@ -11,25 +11,26 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
-@ExtendWith(MockitoExtension.class)
 public class ReviewServiceTest {
 
-    @InjectMocks
+    @Autowired
     private ReviewService reviewService;
 
-    @Mock
+    @MockBean
     private PopupStoreRepository popupStoreRepository;
 
-    @Mock
+    @MockBean
     private User user;
 
-    @Mock
+    @MockBean
     private PopupStore popupStore;
 
     @Test
