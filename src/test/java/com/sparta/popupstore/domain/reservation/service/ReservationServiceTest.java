85f0a75e4ddceeffaccee2ffe610a990348c3cc9
inzulmi <inzulmi132@gmail.com>
2024-12-30 21:01:52 +0900

    [#237] test: mock test

--- src/test/java/com/sparta/popupstore/domain/reservation/service/ReservationServiceTest.java
+++ src/test/java/com/sparta/popupstore/domain/reservation/service/ReservationServiceTest.java
@@ -3,11 +3,6 @@ package com.sparta.popupstore.domain.reservation.service;
 import com.sparta.popupstore.domain.common.exception.CustomApiException;
 import com.sparta.popupstore.domain.common.exception.ErrorCode;
 import com.sparta.popupstore.domain.point.service.PointService;
-import com.sparta.popupstore.domain.popupstore.bundle.entity.PopupStoreAttribute;
-import com.sparta.popupstore.domain.popupstore.bundle.entity.PopupStoreOperating;
-import com.sparta.popupstore.domain.popupstore.bundle.enums.PopupStoreAttributeEnum;
-import com.sparta.popupstore.domain.popupstore.bundle.repository.PopupStoreAttributeRepository;
-import com.sparta.popupstore.domain.popupstore.bundle.repository.PopupStoreOperatingRepository;
 import com.sparta.popupstore.domain.popupstore.bundle.service.PopupStoreBundleService;
 import com.sparta.popupstore.domain.popupstore.entity.PopupStore;
 import com.sparta.popupstore.domain.popupstore.repository.PopupStoreRepository;
@@ -16,104 +11,51 @@ import com.sparta.popupstore.domain.reservation.dto.response.ReservationCreateRe
 import com.sparta.popupstore.domain.reservation.entity.Reservation;
 import com.sparta.popupstore.domain.reservation.repository.ReservationRepository;
 import com.sparta.popupstore.domain.user.entity.User;
-import com.sparta.popupstore.domain.user.repository.UserRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.annotation.DirtiesContext;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.*;
 
-@SpringBootTest
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+
+@ExtendWith(MockitoExtension.class)
 class ReservationServiceTest {
 
-    @Autowired
-    UserRepository userRepository;
-    @Autowired
+    @Mock
     PopupStoreRepository popupStoreRepository;
-    @Autowired
+    @Mock
     ReservationRepository reservationRepository;
-    @Autowired
-    PopupStoreOperatingRepository popupStoreOperatingRepository;
-    @Autowired
-    PopupStoreAttributeRepository popupStoreAttributeRepository;
-    @Autowired
-    PopupStoreBundleService popupStoreBundleService;
-    @MockBean
+    @Mock
     PointService pointService;
+    @Mock
+    PopupStoreBundleService popupStoreBundleService;
 
-    @Autowired
+    @InjectMocks
     ReservationService reservationService;
 
     @Nested
     @Transactional
     @DisplayName("10명 예약 가능한 팝업스토어 예약 인원수 별 테스트")
     class NumberTest {
-
-        Long userId = 1L;
-        Long popupStoreId = 1L;
-        Integer reservationLimit = 10;
-        LocalDate reservationDate = LocalDate.of(2024, 12, 24);
-        LocalTime reservationTime = LocalTime.of(12, 0);
-        Long attributeId = 1L;
-        Long operationId = 1L;
-
-        User user;
-        PopupStore popupStore;
-        PopupStoreAttribute attribute;
-        PopupStoreOperating operation;
-
-        @BeforeEach
-        void setUp() {
-            user = userRepository.save(
-                    User.builder()
-                            .id(userId)
-                            .point(10000)
-                            .build()
-            );
-            popupStore = popupStoreRepository.save(
-                    PopupStore.builder()
-                            .id(popupStoreId)
-                            .price(100)
-                            .reservationLimit(reservationLimit)
-                            .build()
-            );
-            attribute = popupStoreAttributeRepository.save(
-                    PopupStoreAttribute.builder()
-                            .id(attributeId)
-                            .popupStore(popupStore)
-                            .attribute(PopupStoreAttributeEnum.RESERVATION)
-                            .isAllow(true)
-                            .build()
-            );
-            operation = popupStoreOperatingRepository.save(
-                    PopupStoreOperating.builder()
-                            .id(operationId)
-                            .popupStore(popupStore)
-                            .dayOfWeek(reservationDate.getDayOfWeek())
-                            .startTime(reservationTime.minusHours(1))
-                            .endTime(reservationTime.plusHours(1))
-                            .build()
-            );
-        }
-
         @Test
         @Transactional(propagation = Propagation.NESTED)
         @DisplayName("5명 예약")
         void test1() {
             // given
+            LocalDate reservationDate = LocalDate.of(2020, 1, 1);
+            LocalTime reservationTime = LocalTime.of(8, 0);
             Integer number = 5;
             ReservationCreateRequestDto requestDto = new ReservationCreateRequestDto(
                     reservationDate,
@@ -122,7 +64,15 @@ class ReservationServiceTest {
                     null
             );
 
+            User user = getTestUser();
+            Long popupStoreId = 1L;
+            PopupStore popupStore = getTestPopupStore(popupStoreId);
+
+            when(popupStoreRepository.findById(popupStoreId)).thenReturn(Optional.of(popupStore));
+            doNothing().when(popupStoreBundleService).reservationValid(any(), any(), any());
+            when(reservationRepository.findAllByPopupStoreAndReservationDate(any(PopupStore.class), any(LocalDate.class))).thenReturn(List.of());
             doNothing().when(pointService).pointUsed(any(), any(), any(), any());
+            when(reservationRepository.save(any(Reservation.class))).thenReturn(requestDto.toEntity(user, popupStore));
 
             // when
             ReservationCreateResponseDto responseDto = reservationService.createReservation(user, popupStoreId, requestDto);
@@ -139,6 +89,8 @@ class ReservationServiceTest {
         @DisplayName("20명 예약")
         void test2() {
             // given
+            LocalDate reservationDate = LocalDate.of(2020, 1, 1);
+            LocalTime reservationTime = LocalTime.of(8, 0);
             Integer number = 20;
             ReservationCreateRequestDto requestDto = new ReservationCreateRequestDto(
                     reservationDate,
@@ -147,7 +99,13 @@ class ReservationServiceTest {
                     null
             );
 
-            doNothing().when(pointService).pointUsed(any(), any(), any(), any());
+            User user = getTestUser();
+            Long popupStoreId = 1L;
+            PopupStore popupStore = getTestPopupStore(popupStoreId);
+
+            when(popupStoreRepository.findById(popupStoreId)).thenReturn(Optional.of(popupStore));
+            doNothing().when(popupStoreBundleService).reservationValid(any(), any(), any());
+            when(reservationRepository.findAllByPopupStoreAndReservationDate(any(PopupStore.class), any(LocalDate.class))).thenReturn(List.of());
 
             // when & then
             CustomApiException exception = assertThrows(CustomApiException.class,
@@ -160,19 +118,11 @@ class ReservationServiceTest {
         @DisplayName("5명 예약된 상태에서 8명 예약")
         void test3() {
             // given
+            LocalDate reservationDate = LocalDate.of(2020, 1, 1);
+            LocalTime reservationTime = LocalTime.of(8, 0);
             Integer number1 = 5;
             Integer number2 = 8;
 
-            reservationRepository.save(
-                    Reservation.builder()
-                            .id(1L)
-                            .user(user)
-                            .popupStore(popupStore)
-                            .reservationDate(reservationDate)
-                            .reservationTime(reservationTime.withMinute(30))
-                            .number(number1)
-                            .build()
-            );
             ReservationCreateRequestDto requestDto = new ReservationCreateRequestDto(
                     reservationDate,
                     reservationTime,
@@ -180,12 +130,76 @@ class ReservationServiceTest {
                     null
             );
 
-            doNothing().when(pointService).pointUsed(any(), any(), any(), any());
+            User user = getTestUser();
+            Long popupStoreId = 1L;
+            PopupStore popupStore = getTestPopupStore(popupStoreId);
+            Reservation preReservation = getTestReservation(
+                    1L,
+                    user,
+                    popupStore,
+                    reservationDate,
+                    reservationTime,
+                    number1
+            );
+
+            when(popupStoreRepository.findById(popupStoreId)).thenReturn(Optional.of(popupStore));
+            doNothing().when(popupStoreBundleService).reservationValid(any(), any(), any());
+            when(reservationRepository.findAllByPopupStoreAndReservationDate(any(PopupStore.class), any(LocalDate.class))).thenReturn(List.of(preReservation));
 
             // when & then
             CustomApiException exception = assertThrows(CustomApiException.class,
                     () -> reservationService.createReservation(user, popupStoreId, requestDto));
             assertEquals(ErrorCode.RESERVATION_LIMIT_OVER, exception.getErrorCode());
         }
+    }
+
+    private User getTestUser(Long userId, Integer point) {
+        return User.builder()
+                .id(userId)
+                .point(point)
+                .build();
+    }
+    private User getTestUser(Long userId) {
+        return getTestUser(userId, 10000);
+    }
+    private User getTestUser(Integer point) {
+        return getTestUser(1L, point);
+    }
+    private User getTestUser() {
+        return getTestUser(1L, 10000);
+    }
+
+    private PopupStore getTestPopupStore(Long popupStoreId, Integer price, Integer reservationLimit, LocalDate startDate, LocalDate endDate) {
+        return PopupStore.builder()
+                .id(popupStoreId)
+                .price(price)
+                .reservationLimit(reservationLimit)
+                .startDate(startDate)
+                .endDate(endDate)
+                .build();
+    }
+    private PopupStore getTestPopupStore(Long popupStoreId, Integer price, Integer reservationLimit) {
+        LocalDate now = LocalDate.now();
+        return getTestPopupStore(popupStoreId, price, reservationLimit, now.plusDays(1), now.plusMonths(1));
+    }
+    private PopupStore getTestPopupStore(Long popupStoreId, Integer reservationLimit) {
+        return getTestPopupStore(popupStoreId, 100, reservationLimit);
+    }
+    private PopupStore getTestPopupStore(Long popupStoreId) {
+        return getTestPopupStore(popupStoreId, 10);
+    }
+    private PopupStore getTestPopupStore() {
+        return getTestPopupStore(1L);
+    }
+
+    private Reservation getTestReservation(Long reservationId, User user, PopupStore popupStore, LocalDate reservationDate, LocalTime reservationTime, Integer number) {
+        return Reservation.builder()
+                .id(reservationId)
+                .user(user)
+                .popupStore(popupStore)
+                .reservationDate(reservationDate)
+                .reservationTime(reservationTime)
+                .number(number)
+                .build();
     }
 }
