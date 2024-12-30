85a99a64ff5e4a927845ddc401ca5f7f7ba3ecd9
11ioo <11ioo@napalt.co.kr>
2024-12-30 16:35:27 +0900

    [#203] fix : 코드수정

--- src/main/java/com/sparta/popupstore/domain/review/dto/request/ReviewCreateRequestDto.java
+++ src/main/java/com/sparta/popupstore/domain/review/dto/request/ReviewCreateRequestDto.java
@@ -21,9 +21,9 @@ public class ReviewCreateRequestDto {
     private String imageUrl;
 
     private User user;
-    private PopupStore popupStore;
+    public PopupStore popupStore;
 
-    public Review toEntity() {
+    public Review toEntity(User user, PopupStore popupStore) {
         return Review.builder()
                 .user(user)
                 .popupStore(popupStore)
--- src/test/java/com/sparta/popupstore/domain/review/service/ReviewServiceTest.java
+++ src/test/java/com/sparta/popupstore/domain/review/service/ReviewServiceTest.java
@@ -4,17 +4,20 @@ import com.sparta.popupstore.domain.common.exception.CustomApiException;
 import com.sparta.popupstore.domain.common.exception.ErrorCode;
 import com.sparta.popupstore.domain.popupstore.entity.PopupStore;
 import com.sparta.popupstore.domain.popupstore.repository.PopupStoreRepository;
+import com.sparta.popupstore.domain.reservation.repository.ReservationRepository;
 import com.sparta.popupstore.domain.review.dto.request.ReviewCreateRequestDto;
 import com.sparta.popupstore.domain.user.entity.User;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class ReviewServiceTest {
@@ -31,11 +34,20 @@ public class ReviewServiceTest {
     @Mock
     private PopupStore popupStore;
 
+    @Mock
+    private ReservationRepository reservationRepository;
+
     @Test
-    void createReviewTest() {
+    @DisplayName("리뷰생성 테스트")
+    void createReviewTest1() {
         // given
         Long popupStoreId = popupStore.getId();
+        Integer star = 5;
+        String content = " ";
         ReviewCreateRequestDto requestDto = ReviewCreateRequestDto.builder()
+                .user(user)
+                .star(star)
+                .contents(content)
                 .popupStore(popupStore)
                 .build();
 
@@ -47,5 +59,30 @@ public class ReviewServiceTest {
 
         // then
         assertEquals(ErrorCode.POPUP_STORE_NOT_FOUND.getMessage(), exception.getMessage());
+    }
+
+    @Test
+    void createReviewTest2() {
+        // Given
+        Long popupStoreId = popupStore.getId();
+        when(reservationRepository.existsByUserAndPopupStore(user, popupStore)).thenReturn(false);
+        when(popupStoreRepository.findById(popupStoreId)).thenReturn(Optional.of(popupStore));
+        Integer star = 5;
+        String content = " ";
+        ReviewCreateRequestDto requestDto = ReviewCreateRequestDto.builder()
+                .user(user)
+                .star(star)
+                .contents(content)
+                .popupStore(popupStore)
+                .build();
+
+        // When
+        CustomApiException exception = assertThrows(
+                CustomApiException.class,
+                () -> reviewService.createReview(user, popupStoreId, requestDto)
+        );
+
+        // Then
+        assertEquals(ErrorCode.POPUP_STORE_NOT_RESERVATION.getMessage(), exception.getMessage());
     }
 }
